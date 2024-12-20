ae8380d108aecc402cb1e5064a69afcc727f0159
robmeister2020 <thepowerofrob@gmail.com>
2024-12-20 14:47:44 +0000

    moved things around

--- rob-needs-help-server/src/posts-api/crisis-resources-db.js
+++
@@ -1,5 +0,0 @@
-import { executeSqlQuery } from "../db-access/get-db-client.js";
-
-export const getAllCrisisResources = async (response) => {
-    executeSqlQuery('SELECT * FROM CrisisResources', response);
-}
--- rob-needs-help-server/src/posts-api/posts-db.js
+++
@@ -1,9 +0,0 @@
-import { executeSqlQuery } from "../db-access/get-db-client.js";
-
-export const getAllDiaryPosts  = async (response) => {
-    executeSqlQuery('SELECT * FROM BlogPosts WHERE Type = \'Diary\' AND IsShown = 1 ORDER BY BlogId ASC', response);
-}
-
-export const getAllGeneralPosts  = async (response) => {
-    executeSqlQuery('SELECT * FROM BlogPosts WHERE Type = \'General\' AND IsShown = 1 ORDER BY CreatedDate DESC', response);
-}
